952c4af5414aa85ca567ed070a1ca559ae038671
Dachius <53961420+Dachius@users.noreply.github.com>
2022-07-07 20:49:34 -0500

    Corrected error in comment.

--- Euler3.java
+++ Euler3.java
@@ -1,5 +1,5 @@
 /*
- * Euler Problem 2:
+ * Euler Problem 3:
  * The prime factors of 13195 are 5, 7, 13 and 29.
  * 
  * What is the largest prime factor of the number 600851475143 ?
@@ -9,6 +9,7 @@ public class Euler3 {
     public static void main(String[] args){
         long num = 600851475143L, largestPrimeFactor = 0;
 
+        // Run through numbers less than num, and eliminate prime factors in ascending order for efficiency.
         for(long i = 2; i < num; i++){
             if(num % i == 0){
                 num /= i;
