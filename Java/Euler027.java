2f712a34a1a1195b0d5d68f38754fdaf89d9796e
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-20 21:15:23 -0600

    Optimization

--- Java/Euler027.java
+++ Java/Euler027.java
@@ -1,21 +1,19 @@
-
-
 public class Euler027{
 	public static void main(String[] args){
 		long startTime = System.currentTimeMillis();
 		
 		int highestA = 0, highestB = 0, highestN = 0;
 		for(int a = -999; a < 1000; a++){
-			for(int b = -999; b < 1000; b++){
-				boolean toggle = true;
-				for(int n = 0; toggle; n++){
+			for(int b = -1000; b <= 1000; b++){
+				for(int n = 0; true; n++){
 					if(!isPrime(n * n + a * n + b)){
-						toggle = false;
 						if((n - 1) > highestN){
 							highestN = (n - 1);
 							highestA = a;
 							highestB = b;
 						}
+
+                        break;
 					}
 				}
 			}
@@ -24,23 +22,17 @@ public class Euler027{
 		System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #27: " + (highestA * highestB));
 	}
 	
-	public static boolean isPrime(long test){
-		
-		if(test < 1){
-			return false;
-		}
-		
-		if(test == 1){
-			return false;
-		}
-		
-		if(test == 2 || test == 3){
+	// Simple and fast primality test.
+    // Concept is from wikipedia.org/wiki/Primality_test
+	public static boolean isPrime(long n){
+		if(n == 2 || n == 3){
 			return true;
-		}
-		
-		int sqrtTest = (int)Math.sqrt(test);
-		for(long i = 2; i <= sqrtTest; i++){
-			if(test % i == 0){
+		} else if(n <= 1 || n % 2 == 0 || n % 3 == 0){
+            return false;
+        }
+
+		for(long i = 5; i <= (int)Math.sqrt(n); i += 6){
+			if(n % i == 0 || n % (i + 2) == 0){
 				return false;
 			}
 		}
