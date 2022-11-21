475fccbd7e6896fc5ba6b2f4c41ffc04405147a8
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-20 20:45:51 -0600

    Optimization

--- Java/Euler007.java
+++ Java/Euler007.java
@@ -1,26 +1,35 @@
-import java.util.*;
-
+/*
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ * 
+ * What is the 10 001st prime number?
+ */
 public class Euler007{
 	public static void main(String[] args){
         long startTime = System.currentTimeMillis();
         
-		ArrayList<Integer> primes = new ArrayList<Integer>();
+		int num = 2;
 		
-		for(int i = 2; primes.size() <= 10000; i++){
-			
-			if(isPrime(i)){
-				primes.add(i);
+		for(int primes = 0; primes < 10001; num++){
+			if(isPrime(num)){
+				primes++;
 			}
 		}
 		
-		System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #7: " + primes.get(10000));
+		System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #7: " + (num - 1));
 			
 	}
 	
-	public static boolean isPrime(int test){
-		//	This is highly inefficient- I know. ;-;
-		for(int i = 2; i < test; i++){
-			if(test % i == 0){
+    // Simple and fast primality test.
+    // Concept is from wikipedia.org/wiki/Primality_test
+	public static boolean isPrime(long n){
+		if(n == 2 || n == 3){
+			return true;
+		} else if(n <= 1 || n % 2 == 0 || n % 3 == 0){
+            return false;
+        }
+
+		for(long i = 5; i <= (int)Math.sqrt(n); i += 6){
+			if(n % i == 0 || n % (i + 2) == 0){
 				return false;
 			}
 		}
