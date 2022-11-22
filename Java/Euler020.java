0b3230bc6e5b93555e3aed807bae02dde800b3a4
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-22 17:46:43 -0600

    Minor optimization,  clarifying.

--- Java/Euler020.java
+++ Java/Euler020.java
@@ -5,15 +5,12 @@ public class Euler020{
         long startTime = System.currentTimeMillis();
 
 		BigInteger num = new BigInteger("1");
-		
 		for(int i = 2; i <= 100; i++){
-			num = num.multiply(new BigInteger("" + i));
+			num = num.multiply(new BigInteger(Integer.toString(i)));
 		}
-		
 		String numString = num.toString();
 		
 		long sum = 0;
-		
 		for(int i = 0; i < numString.length(); i++){
 			sum += Long.parseLong(numString.substring(i, i + 1));
 		}
