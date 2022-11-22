c869c6779fbc4f0b596a430aabd9c276c18a38bd
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-22 17:24:33 -0600

    Simplifying code.

--- Java/Euler015.java
+++ Java/Euler015.java
@@ -2,31 +2,18 @@ public class Euler015{
 	public static void main(String[] args){
         long startTime = System.currentTimeMillis();
 		
+        // There grid is "actually" 21x21, because we're using the intersections of the borders, not the squares themselves.
 		long[][] cells = new long[21][21];
-		
 		for(int i = 0; i < 21; i++){
 			cells[0][i] = 1;
 			cells[i][0] = 1;
 		}
 		
 		for(int i = 1; i < 21; i++){
-			for(int j = 1; j <= i; j++){
-				cells[i - j + 1][j] = cells[i - j][j] + cells[i - j + 1][j - 1];
-			}
-		}
-		
-		int i = 20, j = 2, jSave = 2;
-		
-		while(cells[20][20] == 0){
-			while(j < 21){
-				cells[i][j] = cells[i - 1][j] + cells[i][j - 1];
-				j++;
-				i--;
-			}
-			i = 20;
-			jSave++;
-			j = jSave;
-		}
+            for(int j = 1; j < 21; j++){
+                cells[i][j] = cells[i][j - 1] + cells[i - 1][j];
+            }
+        }
 		
 		System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #15: " + cells[20][20]);
 	}
