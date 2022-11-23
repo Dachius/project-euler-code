20f04f53962d21f12ba17c6962f9b4f6b294c3c4
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-23 00:58:54 -0600

    Reformating

--- Java/Euler032.java
+++ Java/Euler032.java
@@ -4,8 +4,11 @@ public class Euler032 {
     public static void main(String[] args){
         long startTime = System.currentTimeMillis();
 
-        ArrayList<String> pandigitals = permutations("123456789");
-        ArrayList<Integer> products = new ArrayList<Integer>();
+        List<String> pandigitals = new ArrayList<>();
+        char[] pieces = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        recur(pandigitals, pieces, "");
+
+        HashSet<Integer> products = new HashSet<>();
 
         for(int i = 0; i < 5; i++){
             for(int j = i + 1; j < 8; j++){
@@ -17,66 +20,28 @@ public class Euler032 {
             }
         }
 
-        removeDuplicates(products);
         long sum = 0;
-        for(int i = 0; i < products.size(); i++){
-			sum += products.get(i);
-		}
+        for(Integer n : products){
+            sum += n;
+        }
 
         System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #32: " + sum);
     }
 
-    //Removes duplicate entries from an ArrayList
-	public static void removeDuplicates(ArrayList<Integer> list){
-		for(int i = 0; i < list.size(); i++){
-			Integer temp = list.get(i);
-			list.remove(i);
+    public static void recur(List<String> permutations, char[] pieces, String building){
+        if(pieces.length == 0){
+            permutations.add(building);
+            return;
+        }
 
-			boolean toAdd = true;
+        for(int i = 0; i < pieces.length; i++){
+            recur(permutations, removeAt(pieces, i), building + pieces[i]);
+        }
+    }
 
-			for(int j = 0; j < list.size(); j++){
-				if(list.get(j).equals(temp)){
-					toAdd = false;
-				}
-			}
-
-			if(toAdd){
-				list.add(temp);
-			}
-			else{
-				i--;
-			}
-		}
-	}
-
-    //This finds permutations using recursion
-	public static ArrayList<String> permutations(String s){
-		ArrayList<String> permutationArray = new ArrayList<String>();
-		permutations(permutationArray, s.toCharArray(), "");
-		return permutationArray;
-	}
-
-	private static void permutations(ArrayList<String> building, char[] spares, String workingPermutation){
-		if(spares.length == 0){
-			//Base case:
-			//If no more spares to concatenate recursively, add the complete string 
-			building.add(workingPermutation);
-			return;
-		}
-		else{
-			//Recursive case: 
-			for(int i = 0; i < spares.length; i++){
-				permutations(building, removeAt(spares, i), workingPermutation + spares[i]);
-			}
-			return;
-		}
-	}
-	
-	//Returns a char[] which is the input char[] but with the character at an indicated index removed
-	public static char[] removeAt(char[] charArray, int index){
+    public static char[] removeAt(char[] charArray, int index){
 		char[] toReturn = new char[charArray.length - 1];
 
-		//toSubtract serves to have the index characters are being taken from charArray increase by 1, in order to correctly skip over the character at index
 		int toSubtract = 0;
 
 		for(int i = 0; i < charArray.length; i++){
