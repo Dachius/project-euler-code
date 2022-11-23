690dd9a4dd5afc2f9bcc1bd4edd6b95e1ad9380e
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-22 23:51:24 -0600

    Simplification

--- Java/Euler024.java
+++ Java/Euler024.java
@@ -4,39 +4,28 @@ public class Euler024 {
     public static void main(String[] args){
 		long startTime = System.currentTimeMillis();
 		
-		ArrayList<String> permutations = permutations("0123456789");
+		List<String> permutations = new ArrayList<>();
+        char[] pieces = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        recur(permutations, pieces, "");
+
 		
 		System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #24: " + permutations.get(999999));
     }
 
-    //This finds permutations using recursion
-	public static ArrayList<String> permutations(String s){
-		ArrayList<String> permutationArray = new ArrayList<String>();
-		permutations(permutationArray, s.toCharArray(), "");
-		return permutationArray;
-	}
+    public static void recur(List<String> permutations, char[] pieces, String building){
+        if(pieces.length == 0){
+            permutations.add(building);
+            return;
+        }
 
-	public static void permutations(ArrayList<String> building, char[] spares, String workingPermutation){
-		if(spares.length == 0){
-			//Base case:
-			//If no more spares to concatenate recursively, add the complete string 
-			building.add(workingPermutation);
-			return;
-		}
-		else{
-			//Recursive case: 
-			for(int i = 0; i < spares.length; i++){
-				permutations(building, removeAt(spares, i), workingPermutation + spares[i]);
-			}
-			return;
-		}
+        for(int i = 0; i < pieces.length; i++){
+            recur(permutations, removeAt(pieces, i), building + pieces[i]);
+        }
     }
-    
-    //Returns a char[] which is the input char[] but with the character at an indicated index removed
-	public static char[] removeAt(char[] charArray, int index){
+
+    public static char[] removeAt(char[] charArray, int index){
 		char[] toReturn = new char[charArray.length - 1];
 
-		//toSubtract serves to have the index characters are being taken from charArray increase by 1, in order to correctly skip over the character at index
 		int toSubtract = 0;
 
 		for(int i = 0; i < charArray.length; i++){
