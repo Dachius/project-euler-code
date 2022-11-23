fff3afcd89d871485f0279a097b042843fe0d99d
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-23 00:50:48 -0600

    Reformating

--- Java/Euler031.java
+++ Java/Euler031.java
@@ -1,6 +1,4 @@
 public class Euler031 {
-    public static int[] coins = {200, 100, 50, 20, 10, 5, 2, 1};
-    public static final int TARGET = 200;
     public static void main(String[] args){
         long startTime = System.currentTimeMillis();
 
@@ -9,19 +7,19 @@ public class Euler031 {
         System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #31: " + solution);
     }
 
+    public static final int[] coins = {200, 100, 50, 20, 10, 5, 2, 1};
+    public static final int TARGET = 200;
     public static int coinRecursion(int num, int index){
         if(index == 7){
             return 1;
-        }
-        else if(num == TARGET){
+        } else if(num == TARGET){
             return 1;
         }
-        else{
-            int sum = 0;
+        
+        int sum = 0;
         for(int i = 0; i <= ((TARGET - num) / coins[index]); i++){
             sum += coinRecursion(num + i * coins[index], index + 1);
         }
         return sum;
-        }
     }
 }
