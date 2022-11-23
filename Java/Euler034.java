57820b45876d1a92f0721c436f17e0109aac14ad
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-23 01:15:54 -0600

    Optimization (4x faster)

--- Java/Euler034.java
+++ Java/Euler034.java
@@ -1,19 +1,15 @@
-
-
 public class Euler034{
 	public static void main(String[] args){
 		long startTime = System.currentTimeMillis();
 		
+        // Theoretical maximum is 7 * 9! = 2540160
 		long sum = 0;
 		for(int i = 10; i <= 2540160; i++){
-			String s = "" + i;
 			int tempSum = 0;
-			int[] digits = new int[s.length()];
-			for(int j = 0; j < s.length(); j++){
-				digits[j] = Integer.parseInt(s.substring(j, j + 1));
-			}
+
+            char[] digits = Integer.toString(i).toCharArray();
 			for(int j = 0; j < digits.length; j++){
-				tempSum += factorial(digits[j]);
+				tempSum += factorial(digits[j] - '0');
 			}
 			
 			if(tempSum == i){
@@ -24,13 +20,8 @@ public class Euler034{
 		System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #34: " + sum);
 	}
 	
+    public static int[] factorialTable = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
 	public static int factorial(int n){
-		int temp = 1;
-		
-		for(int i = n; i > 0; i--){
-			temp *= i;
-		}
-		
-		return temp;
+		return factorialTable[n];
 	}
 }
