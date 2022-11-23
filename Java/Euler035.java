d9a9b00405cd390727b5a4990e9f26e866bf9eea
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-23 02:38:18 -0600

    Optimization (20x faster)

--- Java/Euler035.java
+++ Java/Euler035.java
@@ -1,56 +1,60 @@
-
+import java.util.*;
 
 public class Euler035{
 	public static void main(String[] args){
 		long startTime = System.currentTimeMillis();
+        HashSet<Integer> primes = primeSieve(1000000);
+
+        for(Iterator<Integer> iter = primes.iterator(); iter.hasNext();){
+            Integer p = iter.next();
+            char[] digits = p.toString().toCharArray();
+            for(char c : digits){
+                if(c == '0' || c == '2' || c == '4' || c == '5' || c == '6' || c == '8'){
+                    iter.remove();
+                    break;
+                }
+            }
+        }
 		
-		int circularCount = 0;
-		
-		for(int i = 0; i < 1000000; i++){
-			boolean circularCandidate = true;
-			String s = "" + i;
-			int[] digits = new int[s.length()];
-			for(int j = 0; j < s.length(); j++){
-				digits[j] = Integer.parseInt(s.substring(j, j + 1));
-			}
-			for(int j = 0; j < digits.length; j++){
-				int hold = digits[0];
-				for(int k = 0; k < (digits.length - 1); k++){
-					digits[k] = digits[k + 1];
-				}
-				digits[digits.length - 1] = hold;
+		int count = 0;
+		for(Integer p : primes){
+			boolean candidate = true;
+            
+            String num = Integer.toString(p);
+
+			for(int j = 0; j < num.length(); j++){
+				num = num.charAt(num.length() - 1) + num.substring(0, num.length() - 1);
 				
-				String tempString = "";
-				for(int k = 0; k < digits.length; k++){
-					tempString += digits[k];
-				}
-				
-				if(!isPrime(Long.parseLong(tempString))){
-					j = digits.length;
-					circularCandidate = false;
+				if(!primes.contains(Integer.parseInt(num))){
+					candidate = false;
+                    break;
 				}
 			}
 			
-			if(circularCandidate){
-				circularCount++;
+			if(candidate){
+				count++;
 			}
 		}
+
+        // Remember to count previously excluded 2 and 5.
+        count += 2;
 		
-		System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #35: " + circularCount);
+		System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #35: " + count);
 	}
 	
-	public static boolean isPrime(long test){
-		
-		if(test < 2){
-			return false;
-		}
-		
-		int sqrtTest = (int)Math.sqrt(test);
-		for(long i = 2; i <= sqrtTest; i++){
-			if(test % i == 0){
-				return false;
-			}
-		}
-		return true;
-	}
+	// Implement sieve of Eratosthenes
+	public static HashSet<Integer> primeSieve(int n){
+        HashSet<Integer> primes = new HashSet<Integer>();
+        boolean[] nums = new boolean[n + 1];
+        for(long i = 2; i < n; i++){
+            if(!nums[(int)i]){
+                primes.add((int)i);
+                for(long j = i * i; j <= n; j += i){
+                    nums[(int)j] = true;
+                }
+            }
+        }
+
+        return primes;
+    }
 }
