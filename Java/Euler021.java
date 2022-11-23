21f5255f5f82d6798fcdaa242b10a14f303d2ede
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-22 19:12:00 -0600

    Optimization

--- Java/Euler021.java
+++ Java/Euler021.java
@@ -1,25 +1,38 @@
+import java.util.*;
+
 public class Euler021{
     public static void main(String[] args){
         long startTime = System.currentTimeMillis();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
         
         int amicableSum = 0;
-        
         for(int i = 1; i < 10000; i++){
-            if((i == divisorSum(divisorSum(i))) && divisorSum(i) != i){
-                amicableSum += i;
+            int divisorSum = divisorSum(i);
+            if(divisorSum != i){
+                map.put(i, divisorSum(i));
+            }
+
+            if(map.containsKey(divisorSum) && map.get(divisorSum) == i){
+                amicableSum += divisorSum + i;
             }
         }
         System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #21: " + amicableSum);
     }
     
     public static int divisorSum(int num){
-        int tempSum = 0;
-        for(int i = 1; i < num; i++){
+        int tempSum = 0, sqrt = (int)Math.sqrt(num);
+
+        for(int i = 1; i < sqrt; i++){
             if(num % i == 0){
-                tempSum += i;
+                tempSum += i + num/i;
             }
         }
         
+        if(num == sqrt * sqrt){
+            tempSum += sqrt;
+        }
+
         return tempSum;
     }
 }
