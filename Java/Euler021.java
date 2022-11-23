2f9a52fad1ca521032d59b733ff000455d12b80d
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-22 19:33:04 -0600

    Optimization, bugfix

--- Java/Euler021.java
+++ Java/Euler021.java
@@ -1,20 +1,13 @@
-import java.util.*;
-
 public class Euler021{
     public static void main(String[] args){
         long startTime = System.currentTimeMillis();
-
-        HashMap<Integer, Integer> map = new HashMap<>();
         
         int amicableSum = 0;
+        
         for(int i = 1; i < 10000; i++){
-            int divisorSum = divisorSum(i);
-            if(divisorSum != i){
-                map.put(i, divisorSum(i));
-            }
-
-            if(map.containsKey(divisorSum) && map.get(divisorSum) == i){
-                amicableSum += divisorSum + i;
+            int iSum = divisorSum(i);
+            if((i == divisorSum(iSum)) && iSum != i){
+                amicableSum += i;
             }
         }
         System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #21: " + amicableSum);
@@ -23,16 +16,16 @@ public class Euler021{
     public static int divisorSum(int num){
         int tempSum = 0, sqrt = (int)Math.sqrt(num);
 
-        for(int i = 1; i < sqrt; i++){
+        for(int i = 2; i < sqrt; i++){
             if(num % i == 0){
-                tempSum += i + num/i;
+                tempSum += (i + num/i);
             }
         }
         
-        if(num == sqrt * sqrt){
+        if(num == sqrt * sqrt && num != 1){
             tempSum += sqrt;
         }
 
-        return tempSum;
+        return tempSum + 1;
     }
 }
