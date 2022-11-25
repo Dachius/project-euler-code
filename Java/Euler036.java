50b9a2c425588e1a54aa9e0b1464287fc3cf6cfb
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-25 10:50:32 -0600

    Optimization (1.5x faster)

--- Java/Euler036.java
+++ Java/Euler036.java
@@ -1,13 +1,15 @@
-
+/*
+ * There's room for extreme optimization on this problem. In fact, we need only check 1110 different possibilities
+ * due to various mathematical results on the form of decimal fractions. However, this seems like vast overkill.
+ */
 
 public class Euler036{
 	public static void main(String[] args){
 		long startTime = System.currentTimeMillis();
         
 		long sum = 0;
-		
-		for(int i = 1; i < 1000000; i ++){
-			if(isPalindrome("" + i) && isPalindrome(Integer.toBinaryString(i))){
+		for(int i = 1; i < 1000000; i += 2){
+			if(isPalindrome(Integer.toString(i)) && isPalindrome(Integer.toBinaryString(i))){
 				sum += i;
 			}
 		}
