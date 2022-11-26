93ba996db9ad6781271dc533a63777a60589bb5f
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-26 17:28:07 -0600

    Optimizated primality test

--- Java/Euler037.java
+++ Java/Euler037.java
@@ -1,5 +1,3 @@
-
-
 public class Euler037{
 	public static void main(String[] args){
 		long startTime = System.currentTimeMillis();
@@ -14,29 +12,31 @@ public class Euler037{
 		
 		System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #37: " + sum);
 	}
-	
+
 	public static boolean isTruncatable(int test){
 		String s = "" + test;
 		for(int i = s.length() - 1; i >= 0; i--){
-			if(!(isPrime(Integer.parseInt(s.substring(i, s.length()))) && isPrime(Integer.parseInt(s.substring(0, s.length() - i))))){
+			if(!(isPrime(Integer.parseInt(s.substring(0, s.length() - i))) && isPrime(Integer.parseInt(s.substring(i, s.length()))))){
 				return false;
 			}
 		}
 		return true;
 	}
 
-    public static boolean isPrime(long test){
-        
-        if(test < 2){
+    // Simple and fast primality test.
+    // Concept is from wikipedia.org/wiki/Primality_test
+	public static boolean isPrime(long n){
+		if(n == 2 || n == 3){
+			return true;
+		} else if(n <= 1 || n % 2 == 0 || n % 3 == 0){
             return false;
         }
-        
-        int sqrtTest = (int)Math.sqrt(test);
-        for(long i = 2; i <= sqrtTest; i++){
-            if(test % i == 0){
-                return false;
-            }
-        }
-        return true;
-    }
+
+		for(long i = 5; i <= (int)Math.sqrt(n); i += 6){
+			if(n % i == 0 || n % (i + 2) == 0){
+				return false;
+			}
+		}
+		return true;
+	}
 }
