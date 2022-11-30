644f7e43dba16fa90ad6910f6d36660721cba7fe
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-29 23:38:22 -0600

    reformatting

--- Java/Euler038.java
+++ Java/Euler038.java
@@ -32,8 +32,6 @@ public class Euler038 {
         ArrayList<Long> building = new ArrayList<Long>();
 
         for(int i = 0; i < list.size(); i++){
-            //if statement is problem specific
-            //remove if statement if not using for super similar application
             if(!(list.get(i).charAt(0) == '0')){
                 building.add(Long.parseLong(list.get(i)));
             }
@@ -42,7 +40,6 @@ public class Euler038 {
         return building;
     }
 
-    //This finds permutations using recursion
 	public static ArrayList<String> permutations(String s){
 		ArrayList<String> permutationArray = new ArrayList<String>();
 		permutations(permutationArray, s.toCharArray(), "");
@@ -51,13 +48,9 @@ public class Euler038 {
 
 	private static void permutations(ArrayList<String> building, char[] spares, String workingPermutation){
 		if(spares.length == 0){
-			//Base case:
-			//If no more spares to concatenate recursively, add the complete string 
 			building.add(workingPermutation);
 			return;
-		}
-		else{
-			//Recursive case: 
+		} else{
 			for(int i = 0; i < spares.length; i++){
 				permutations(building, removeAt(spares, i), workingPermutation + spares[i]);
 			}
@@ -65,11 +58,9 @@ public class Euler038 {
 		}
 	}
 	
-	//Returns a char[] which is the input char[] but with the character at an indicated index removed
 	public static char[] removeAt(char[] charArray, int index){
 		char[] toReturn = new char[charArray.length - 1];
 
-		//toSubtract serves to have the index characters are being taken from charArray increase by 1, in order to correctly skip over the character at index
 		int toSubtract = 0;
 
 		for(int i = 0; i < charArray.length; i++){
