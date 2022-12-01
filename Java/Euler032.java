6daf844c10bb2c01751932e802ca5108eff8f8fc
Dachius <53961420+Dachius@users.noreply.github.com>
2022-12-01 00:25:18 -0600

    30% less code, 20% slower

--- Java/Euler032.java
+++ Java/Euler032.java
@@ -5,16 +5,16 @@ public class Euler032 {
         long startTime = System.currentTimeMillis();
 
         List<String> pandigitals = new ArrayList<>();
-        char[] pieces = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-        recur(pandigitals, pieces, "");
+        Character[] vals = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        recur(pandigitals, Arrays.asList(vals), "");
 
         HashSet<Integer> products = new HashSet<>();
 
         for(int i = 0; i < 5; i++){
             for(int j = i + 1; j < 8; j++){
-                for(int k = 0; k < pandigitals.size(); k++){
-                    if(Integer.parseInt(pandigitals.get(k).substring(0, i + 1)) * Integer.parseInt(pandigitals.get(k).substring(i + 1, j + 1)) == Integer.parseInt(pandigitals.get(k).substring(j + 1, 9))){
-                        products.add(Integer.parseInt(pandigitals.get(k).substring(j + 1, 9)));
+                for(String s : pandigitals){
+                    if(Integer.parseInt(s.substring(0, i + 1)) * Integer.parseInt(s.substring(i + 1, j + 1)) == Integer.parseInt(s.substring(j + 1, 9))){
+                        products.add(Integer.parseInt(s.substring(j + 1, 9)));
                     }
                 }
             }
@@ -28,31 +28,15 @@ public class Euler032 {
         System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #32: " + sum);
     }
 
-    public static void recur(List<String> permutations, char[] pieces, String building){
-        if(pieces.length == 0){
+    public static void recur(List<String> permutations, List<Character> pieces, String building){
+        if(pieces.size() == 0){
             permutations.add(building);
-            return;
-        }
-
-        for(int i = 0; i < pieces.length; i++){
-            recur(permutations, removeAt(pieces, i), building + pieces[i]);
+        } else{
+            for(Character c : pieces){
+                List<Character> pieceClone = new ArrayList<>(pieces);
+                pieceClone.remove(c);
+                recur(permutations, pieceClone, building + c);
+            }
         }
     }
-
-    public static char[] removeAt(char[] charArray, int index){
-		char[] toReturn = new char[charArray.length - 1];
-
-		int toSubtract = 0;
-
-		for(int i = 0; i < charArray.length; i++){
-			if(i != index){
-				toReturn[i - toSubtract] = charArray[i];
-			}
-			else{
-				toSubtract++;
-			}
-		}
-
-		return toReturn;
-	}
 }
