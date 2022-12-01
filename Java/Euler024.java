b6f3beb0e53e235e5410742ec3b652de5b9ac497
Dachius <53961420+Dachius@users.noreply.github.com>
2022-12-01 01:21:01 -0600

    Simplification

--- Java/Euler024.java
+++ Java/Euler024.java
@@ -5,38 +5,22 @@ public class Euler024 {
 		long startTime = System.currentTimeMillis();
 		
 		List<String> permutations = new ArrayList<>();
-        char[] pieces = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-        recur(permutations, pieces, "");
+        Character[] pieces = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        recur(permutations, Arrays.asList(pieces), "");
 
 		
 		System.out.println("[" + (System.currentTimeMillis() - startTime) + "ms] Solution #24: " + permutations.get(999999));
     }
 
-    public static void recur(List<String> permutations, char[] pieces, String building){
-        if(pieces.length == 0){
+    public static void recur(List<String> permutations, List<Character> pieces, String building){
+        if(pieces.size() == 0){
             permutations.add(building);
-            return;
-        }
-
-        for(int i = 0; i < pieces.length; i++){
-            recur(permutations, removeAt(pieces, i), building + pieces[i]);
+        } else{
+            for(Character c : pieces){
+                List<Character> pieceClone = new ArrayList<>(pieces);
+                pieceClone.remove(c);
+                recur(permutations, pieceClone, building + c);
+            }
         }
     }
-
-    public static char[] removeAt(char[] charArray, int index){
-		char[] toReturn = new char[charArray.length - 1];
-
-		int toSubtract = 0;
-
-		for(int i = 0; i < charArray.length; i++){
-			if(i != index){
-				toReturn[i - toSubtract] = charArray[i];
-			}
-			else{
-				toSubtract++;
-			}
-		}
-
-		return toReturn;
-	}
 }
