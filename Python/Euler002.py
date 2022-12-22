699fd5376f14c5f1a90683f63b670f9851355417
Dachius <53961420+Dachius@users.noreply.github.com>
2022-12-22 16:00:55 -0600

    Python #2

---
+++ Python/Euler002.py
@@ -0,0 +1,16 @@
+import time
+
+start_time = time.time()
+
+sum = 0
+fibs = [0, 1, 1]
+while fibs[2] < 4000000:
+    if fibs[2] % 2 == 0:
+        sum += fibs[2]
+
+    # shift nums over, generate next fibonacci number
+    fibs[0] = fibs[1]
+    fibs[1] = fibs[2]
+    fibs[2] = fibs[0] + fibs[1]
+
+print("[" + str(int(time.time() - start_time)) + "ms] Solution #2: " + str(sum))
